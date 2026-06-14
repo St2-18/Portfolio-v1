5dae0a5e4f198b87d444e2203a8a0a62811cb083
Harshit Sharma <harshitsharma77117@gmail.com>
2026-06-15 00:19:32 +0530

    fix: mobile full-width layout and overflow issues

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -19,7 +19,7 @@ const Index = () => {
   }, []);
 
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-x-hidden w-full relative">
       <Navigation />
 
       <main>
